e006c3aa0b053bf8242b71f671fa59feff8abed6
prasaduchukka <prasadchukka630@gmail.com>
2026-08-15 22:37:20 +0530

    Replace old frontend files with updated version

--- svp-v3/serve.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ================================
-echo  Smart Village Portal v3
-echo  Telugu + English + Responsive
-echo  Open: http://localhost:3000
-echo ================================
-python -m http.server 3000
